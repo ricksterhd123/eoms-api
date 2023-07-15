b18f527457f4f312a73378d9230a9f1e727ee8e1
ricksterhd123 <ricksterhd123@gmail.com>
2023-07-15 01:12:43 +0100

    Comment out tables in sql config

--- postgres/eoms.sql
+++ postgres/eoms.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE eoms;
-\connect eoms
+-- \connect eoms
 
 -- DROP TABLE IF EXISTS "order_item";
 -- DROP TABLE IF EXISTS "item";
@@ -27,52 +27,24 @@ CREATE DATABASE eoms;
 --     `password` TEXT
 -- );
 
---
--- Customers
---
+-- --
+-- -- Orders
+-- --
 
-CREATE TABLE customer(
-    id SERIAL PRIMARY KEY,
-    customer_reference TEXT,
-    customer_name TEXT,
-    customer_email_address TEXT,
-    updated_on TIMESTAMPTZ,
-    created_on TIMESTAMPTZ
-);
+-- CREATE TABLE "order"(
+--     id SERIAL PRIMARY KEY,
+--     order_customer_id SERIAL,
+--     order_reference TEXT,
+--     order_status TEXT,
+--     updated_on TIMESTAMPTZ,
+--     created_on TIMESTAMPTZ,
+--     FOREIGN KEY(order_customer_id) REFERENCES customer(id)
+-- );
 
---
--- Items
---
-
-CREATE TABLE item(
-    id SERIAL PRIMARY KEY,
-    item_sku TEXT,
-    item_reference TEXT,
-    item_description TEXT,
-    item_unit_price NUMERIC,
-    item_stock_quantity NUMERIC,
-    updated_on TIMESTAMPTZ,
-    created_on TIMESTAMPTZ
-);
-
---
--- Orders
---
-
-CREATE TABLE "order"(
-    id SERIAL PRIMARY KEY,
-    order_customer_id SERIAL,
-    order_reference TEXT,
-    order_status TEXT,
-    updated_on TIMESTAMPTZ,
-    created_on TIMESTAMPTZ,
-    FOREIGN KEY(order_customer_id) REFERENCES customer(id)
-);
-
-CREATE TABLE order_item(
-    id SERIAL PRIMARY KEY,
-    order_id SERIAL,
-    item_id SERIAL,
-    FOREIGN KEY(order_id) REFERENCES "order"(id),
-    FOREIGN KEY(item_id) REFERENCES item(id)
-);
+-- CREATE TABLE order_item(
+--     id SERIAL PRIMARY KEY,
+--     order_id SERIAL,
+--     item_id SERIAL,
+--     FOREIGN KEY(order_id) REFERENCES "order"(id),
+--     FOREIGN KEY(item_id) REFERENCES item(id)
+-- );
