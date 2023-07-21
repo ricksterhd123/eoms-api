5f02f7df8edd6cdec7e4723c7bd0d1b181685a2a
ricksterhd123 <ricksterhd123@gmail.com>
2023-07-21 19:22:46 +0100

    Add Order and OrderItem model

--- eoms-api/models.py
+++ eoms-api/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Integer, Numeric, Text, Time
+from sqlalchemy import Integer, Numeric, Text, Time, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -17,7 +17,7 @@ class Item(Base):
     created_on: Mapped[datetime] = mapped_column(Time())
 
     def __repr__(self):
-        return f"Item(id={self.id!r}, sku={self.sku!r}, description={self.description!r})"
+        return f"Item(id={self.id!r}, sku={self.sku!r}, reference={self.reference!r}, description={self.description!r}, unit_price={self.unit_price!r}, stock_quantity={self.stock_quantity!r}, updated_on={self.updated_on!r}, created_on={self.created_on!r})"
 
 class Customer(Base):
     __tablename__ = "customer"
@@ -29,4 +29,27 @@ class Customer(Base):
     created_on: Mapped[datetime] = mapped_column(Time())
 
     def __repr__(self):
-        return f"Customer(id={self.id!r}, name={self.name!r}, email_address={self.email_address!r})"
+        return f"Customer(id={self.id!r}, reference={self.reference!r}, name={self.name!r}, email_address={self.email_address!r}, updated_on={self.updated_on!r}, created_on={self.updated_on!r})"
+
+class OrderItem(Base):
+    __tablename__ = "order_item"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement="auto")
+    order_id: Mapped[int] = mapped_column(ForeignKey("order.id"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id"))
+    updated_on: Mapped[datetime] = mapped_column(Time())
+    created_on: Mapped[datetime] = mapped_column(Time())
+
+    def __repr__(self):
+        return f"OrderItem(id={self.id!r}, order_id={self.order_id!r}, item_id={self.item_id!r}, updated_on={self.updated_on!r}, created_on={self.created_on!r})"
+
+class Order(Base):
+    __tablename__ = "order"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement="auto")
+    customer_id: Mapped[int] = mapped_column(ForeignKey("customer.id"))
+    reference: Mapped[str] = mapped_column(Text())
+    status: Mapped[str] = mapped_column(Text())
+    updated_on: Mapped[datetime] = mapped_column(Time())
+    created_on: Mapped[datetime] = mapped_column(Time())
+
+    def __repr__(self):
+        return f"Order(id={self.id!r}, customer_id={self.item_id!r}, reference={self.updated_on!r}, status={self.created_on!r}, updated_on={self.updated_on!r}, created_on={self.created_on!r})"
